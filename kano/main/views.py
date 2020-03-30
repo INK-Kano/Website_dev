4ca0d8eef364fba5a3ba083e9a9cd953b5d7037a
KANO <user1205@naver.com>
2020-03-30 17:12:59 +0900

    img upload test

--- kano/main/views.py
+++ kano/main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def main(request):
     return render(request, 'main.html')
+
+
+def a(request):
+    name = 'name'
+    return render({"name": name})
